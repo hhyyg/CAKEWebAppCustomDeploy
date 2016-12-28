20ea8c157b6f0811a36d02cb77b2811547120020
hiroka.85 <hiroka.85>
2016-12-28 15:05:58 +0900

    init

---
+++ MisoDep/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MisoDep.Startup))]
+namespace MisoDep
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
